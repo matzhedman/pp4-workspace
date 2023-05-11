fb905a2f8d46aebc4c49579b21b9a6ca7b43eccc
matzhedman <>
2023-05-11 06:24:43 +0000

    Add userform to forms.py

--- studio/forms.py
+++ studio/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from .models import Offer
 
 
+# datepicker
 class DateInput(forms.DateInput):
     input_type = 'date'
 
@@ -14,3 +15,10 @@ class OfferForm(forms.ModelForm):
         widgets = {
             'expiration_date': DateInput(),
         }
+
+
+class UserForm(forms.Form):
+    first_name = forms.CharField(max_length = 35)
+    last_name = forms.Charfield(max_length = 35)
+    email_address = forms.EmailField(max_length = 150)
+    message = forms.CharField(widget = forms.Textarea, max_length = 500)
